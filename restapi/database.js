43d74650ca58cdaf5e291602fd96145f6060d28f
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2021-04-27 12:24:23 +0300

    Muokattu tilitietojen hakua ja kortin lukitusta

--- restapi/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'asiakas',
-  password: 'test',
-  database: 'testi'
-});
-module.exports = connection;
--- restapi/models/login_model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database');
-
-const login={
-  checkPinkoodi: function(Tunnus_kortti, callback) {
-      return db.query('SELECT Pin_koodi FROM kortti WHERE Tunnus_Kortti = ?',[Tunnus_kortti], callback); 
-    }
-};
-          
-module.exports = login;
